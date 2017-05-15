31ffc525c3e08b2eb272ba610a65f94540ab42cf
Adam Bretz <arbretz@gmail.com>
2017-05-15 10:24:23 -0400

    Revert "🚧 - TypeScript setup. Extension code."
    This reverts commit 40198f5a2f6fa36a5ecb9e064e6b2da857a2eaa1.

--- stardog-rdf-grammars/src/extension.ts
+++
@@ -1,8 +0,0 @@
-import * as vscode from 'vscode';
-import SPARQLFormatter from './providers/sparqlformatter';
-
-export function activate(context: vscode.ExtensionContext) {
-  console.log('activate');
-  context.subscriptions.push(
-    vscode.languages.registerDocumentFormattingEditProvider(['sparql'], new SPARQLFormatter()));
-};
