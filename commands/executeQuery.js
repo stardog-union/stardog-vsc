6081c1a214c45a64cbf9a46a0b2577eae34a0b07
Ty Soehngen <ty@stardog.com>
2017-11-13 16:44:41 -0500

    First attempt at opening query results in split pane

--- commands/executeQuery.js
+++ commands/executeQuery.js
@@ -6,14 +6,38 @@ module.exports = (config, resultProvider) => {
   const connection = new Connection(config);
 
   console.log(connection);
-  query.execute(connection, 'myDB', 'select distinct ?s where { ?s ?p ?o }', { limit: 1 })
-    .then(({ body }) => {
-      resultProvider.setData(body.head.vars, body.results.bindings);
-      const uri = vscode.Uri.parse(`stardog-results://${'myDB'}/results`);
 
-      vscode.commands.executeCommand('vscode.previewHtml', uri, vscode.ViewColumn.One, 'Query Results')
-        .then(() => { }, (reason) => {
-          vscode.window.showErrorMessage(reason);
-        });
-    });
+
+  const q = `select distinct ?entity ?elabel ?type ?tlabel 
+                                      where { 
+                                        ?entity a ?type . 
+                                        OPTIONAL { ?entity rdfs:label ?elabel } . 
+                                        OPTIONAL { ?type rdfs:label ?tlabel } 
+                                      }`;
+  vscode.commands.executeCommand('vscode.moveActiveEditor', { to: 'first', by: 'group'})
+  .then(() => { }, (err) => console.err)
+  .then(() => query.execute(connection, 'exercise', q.split('\n').join(''), { limit: 1 })
+    .then(({ body }) => {
+      
+      
+      resultProvider.setData(body.head.vars, body.results.bindings);
+      const uri = vscode.Uri.parse(`stardog-results://${'exercise'}/results`);
+
+      return vscode.commands.executeCommand('vscode.previewHtml', uri, vscode.ViewColumn.One, 'Query Results')
+        .then(() => {
+          
+          
+          vscode.commands.executeCommand(
+            'vscode.moveActiveEditor',
+            {
+              to: 'last',
+              by: 'group',
+            }
+          )
+        })
+    })
+    .then(() => { }, (reason) => {
+        vscode.window.showErrorMessage(reason);
+    })
+  )
 }
