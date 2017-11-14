416dd87983734b36557b6afdf47e89d389715d98
Jason Rogers <jason@stardog.com>
2017-11-14 07:58:50 -0500

    Make query results show in split pane view

--- commands/executeQuery.js
+++ commands/executeQuery.js
@@ -5,39 +5,19 @@ const { query, Connection } = require('stardog');
 module.exports = (config, resultProvider) => {
   const connection = new Connection(config);
 
-  console.log(connection);
-
-
   const q = `select distinct ?entity ?elabel ?type ?tlabel 
                                       where { 
                                         ?entity a ?type . 
                                         OPTIONAL { ?entity rdfs:label ?elabel } . 
                                         OPTIONAL { ?type rdfs:label ?tlabel } 
                                       }`;
-  vscode.commands.executeCommand('vscode.moveActiveEditor', { to: 'first', by: 'group'})
-  .then(() => { }, (err) => console.err)
-  .then(() => query.execute(connection, 'exercise', q.split('\n').join(''), { limit: 1 })
+  query.execute(connection, 'myDB', q.split('\n').join(''), { limit: 1 })
     .then(({ body }) => {
-      
-      
       resultProvider.setData(body.head.vars, body.results.bindings);
-      const uri = vscode.Uri.parse(`stardog-results://${'exercise'}/results`);
-
-      return vscode.commands.executeCommand('vscode.previewHtml', uri, vscode.ViewColumn.One, 'Query Results')
-        .then(() => {
-          
-          
-          vscode.commands.executeCommand(
-            'vscode.moveActiveEditor',
-            {
-              to: 'last',
-              by: 'group',
-            }
-          )
-        })
+      const uri = vscode.Uri.parse(`stardog-results://${'myDB'}/results`);
+      return vscode.commands.executeCommand('vscode.previewHtml', uri, vscode.ViewColumn.Two, 'Query Results');
     })
     .then(() => { }, (reason) => {
         vscode.window.showErrorMessage(reason);
-    })
-  )
+    });
 }
