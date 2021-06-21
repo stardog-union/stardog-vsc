cdf4ff2570fd62cb862498791a0b40f904e2968e
Jason Rogers <jason@stardog.com>
2021-06-21 14:27:47 -0400

    Fix SPARQL extension tests

--- vscode-langserver-sparql/out/test/extension.test.js
+++ vscode-langserver-sparql/out/test/extension.test.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -15,7 +16,7 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 describe("SPARQL Language Server Extension", () => {
     let docUri;
     let document;
-    beforeEach(() => __awaiter(this, void 0, void 0, function* () {
+    beforeEach(() => __awaiter(void 0, void 0, void 0, function* () {
         const ext = vscode.extensions.getExtension("stardog-union.vscode-langserver-sparql");
         yield ext.activate();
         docUri = vscode.Uri.file(path.join(__dirname, "..", "..", "fixtures", "bad", "basic-bad-select.sparql"));
@@ -46,7 +47,7 @@ describe("SPARQL Language Server Extension", () => {
                 source: "GroupGraphPattern"
             },
             {
-                message: "\tExpected one of the following:\n '^'\n IRIREF e.g. <http://example.com>\n PNAME_LN\n PNAME_NS\n 'a'\n '!'\n '('\n VAR1 e.g. ?foo\n VAR2 e.g. ?bar",
+                message: "\tExpected one of the following:\n '^'\n '!'\n '('\n IRIREF e.g. <http://example.com> '?'\n IRIREF e.g. <http://example.com> '*'\n IRIREF e.g. <http://example.com> '+'\n IRIREF e.g. <http://example.com> '/'\n IRIREF e.g. <http://example.com> '|'\n IRIREF e.g. <http://example.com> VAR1 e.g. ?foo\n IRIREF e.g. <http://example.com> VAR2 e.g. ?bar\n IRIREF e.g. <http://example.com> IRIREF e.g. <http://example.com>\n IRIREF e.g. <http://example.com> PNAME_LN\n IRIREF e.g. <http://example.com> PNAME_NS\n IRIREF e.g. <http://example.com> STRING_LITERAL1\n IRIREF e.g. <http://example.com> STRING_LITERAL2\n IRIREF e.g. <http://example.com> STRING_LITERAL_LONG1\n IRIREF e.g. <http://example.com> STRING_LITERAL_LONG2\n IRIREF e.g. <http://example.com> INTEGER\n IRIREF e.g. <http://example.com> DECIMAL\n IRIREF e.g. <http://example.com> DOUBLE\n IRIREF e.g. <http://example.com> INTEGER_POSITIVE\n IRIREF e.g. <http://example.com> DECIMAL_POSITIVE\n IRIREF e.g. <http://example.com> DOUBLE_POSITIVE\n IRIREF e.g. <http://example.com> INTEGER_NEGATIVE\n IRIREF e.g. <http://example.com> DECIMAL_NEGATIVE\n IRIREF e.g. <http://example.com> DOUBLE_NEGATIVE\n IRIREF e.g. <http://example.com> 'TRUE'\n IRIREF e.g. <http://example.com> 'FALSE'\n IRIREF e.g. <http://example.com> BLANK_NODE_LABEL\n IRIREF e.g. <http://example.com> ANON e.g. []\n IRIREF e.g. <http://example.com> NIL e.g. ()\n IRIREF e.g. <http://example.com> '<<'\n IRIREF e.g. <http://example.com> '('\n IRIREF e.g. <http://example.com> '['\n PNAME_LN '?'\n PNAME_LN '*'\n PNAME_LN '+'\n PNAME_LN '/'\n PNAME_LN '|'\n PNAME_LN VAR1 e.g. ?foo\n PNAME_LN VAR2 e.g. ?bar\n PNAME_LN IRIREF e.g. <http://example.com>\n PNAME_LN PNAME_LN\n PNAME_LN PNAME_NS\n PNAME_LN STRING_LITERAL1\n PNAME_LN STRING_LITERAL2\n PNAME_LN STRING_LITERAL_LONG1\n PNAME_LN STRING_LITERAL_LONG2\n PNAME_LN INTEGER\n PNAME_LN DECIMAL\n PNAME_LN DOUBLE\n PNAME_LN INTEGER_POSITIVE\n PNAME_LN DECIMAL_POSITIVE\n PNAME_LN DOUBLE_POSITIVE\n PNAME_LN INTEGER_NEGATIVE\n PNAME_LN DECIMAL_NEGATIVE\n PNAME_LN DOUBLE_NEGATIVE\n PNAME_LN 'TRUE'\n PNAME_LN 'FALSE'\n PNAME_LN BLANK_NODE_LABEL\n PNAME_LN ANON e.g. []\n PNAME_LN NIL e.g. ()\n PNAME_LN '<<'\n PNAME_LN '('\n PNAME_LN '['\n PNAME_NS '?'\n PNAME_NS '*'\n PNAME_NS '+'\n PNAME_NS '/'\n PNAME_NS '|'\n PNAME_NS VAR1 e.g. ?foo\n PNAME_NS VAR2 e.g. ?bar\n PNAME_NS IRIREF e.g. <http://example.com>\n PNAME_NS PNAME_LN\n PNAME_NS PNAME_NS\n PNAME_NS STRING_LITERAL1\n PNAME_NS STRING_LITERAL2\n PNAME_NS STRING_LITERAL_LONG1\n PNAME_NS STRING_LITERAL_LONG2\n PNAME_NS INTEGER\n PNAME_NS DECIMAL\n PNAME_NS DOUBLE\n PNAME_NS INTEGER_POSITIVE\n PNAME_NS DECIMAL_POSITIVE\n PNAME_NS DOUBLE_POSITIVE\n PNAME_NS INTEGER_NEGATIVE\n PNAME_NS DECIMAL_NEGATIVE\n PNAME_NS DOUBLE_NEGATIVE\n PNAME_NS 'TRUE'\n PNAME_NS 'FALSE'\n PNAME_NS BLANK_NODE_LABEL\n PNAME_NS ANON e.g. []\n PNAME_NS NIL e.g. ()\n PNAME_NS '<<'\n PNAME_NS '('\n PNAME_NS '['\n 'a' '?'\n 'a' '*'\n 'a' '+'\n 'a' '/'\n 'a' '|'\n 'a' VAR1 e.g. ?foo\n 'a' VAR2 e.g. ?bar\n 'a' IRIREF e.g. <http://example.com>\n 'a' PNAME_LN\n 'a' PNAME_NS\n 'a' STRING_LITERAL1\n 'a' STRING_LITERAL2\n 'a' STRING_LITERAL_LONG1\n 'a' STRING_LITERAL_LONG2\n 'a' INTEGER\n 'a' DECIMAL\n 'a' DOUBLE\n 'a' INTEGER_POSITIVE\n 'a' DECIMAL_POSITIVE\n 'a' DOUBLE_POSITIVE\n 'a' INTEGER_NEGATIVE\n 'a' DECIMAL_NEGATIVE\n 'a' DOUBLE_NEGATIVE\n 'a' 'TRUE'\n 'a' 'FALSE'\n 'a' BLANK_NODE_LABEL\n 'a' ANON e.g. []\n 'a' NIL e.g. ()\n 'a' '<<'\n 'a' '('\n 'a' '['\n VAR1 e.g. ?foo VAR1 e.g. ?foo\n VAR1 e.g. ?foo VAR2 e.g. ?bar\n VAR1 e.g. ?foo IRIREF e.g. <http://example.com>\n VAR1 e.g. ?foo PNAME_LN\n VAR1 e.g. ?foo PNAME_NS\n VAR1 e.g. ?foo STRING_LITERAL1\n VAR1 e.g. ?foo STRING_LITERAL2\n VAR1 e.g. ?foo STRING_LITERAL_LONG1\n VAR1 e.g. ?foo STRING_LITERAL_LONG2\n VAR1 e.g. ?foo INTEGER\n VAR1 e.g. ?foo DECIMAL\n VAR1 e.g. ?foo DOUBLE\n VAR1 e.g. ?foo INTEGER_POSITIVE\n VAR1 e.g. ?foo DECIMAL_POSITIVE\n VAR1 e.g. ?foo DOUBLE_POSITIVE\n VAR1 e.g. ?foo INTEGER_NEGATIVE\n VAR1 e.g. ?foo DECIMAL_NEGATIVE\n VAR1 e.g. ?foo DOUBLE_NEGATIVE\n VAR1 e.g. ?foo 'TRUE'\n VAR1 e.g. ?foo 'FALSE'\n VAR1 e.g. ?foo BLANK_NODE_LABEL\n VAR1 e.g. ?foo ANON e.g. []\n VAR1 e.g. ?foo NIL e.g. ()\n VAR1 e.g. ?foo '<<'\n VAR1 e.g. ?foo '('\n VAR1 e.g. ?foo '['\n VAR2 e.g. ?bar VAR1 e.g. ?foo\n VAR2 e.g. ?bar VAR2 e.g. ?bar\n VAR2 e.g. ?bar IRIREF e.g. <http://example.com>\n VAR2 e.g. ?bar PNAME_LN\n VAR2 e.g. ?bar PNAME_NS\n VAR2 e.g. ?bar STRING_LITERAL1\n VAR2 e.g. ?bar STRING_LITERAL2\n VAR2 e.g. ?bar STRING_LITERAL_LONG1\n VAR2 e.g. ?bar STRING_LITERAL_LONG2\n VAR2 e.g. ?bar INTEGER\n VAR2 e.g. ?bar DECIMAL\n VAR2 e.g. ?bar DOUBLE\n VAR2 e.g. ?bar INTEGER_POSITIVE\n VAR2 e.g. ?bar DECIMAL_POSITIVE\n VAR2 e.g. ?bar DOUBLE_POSITIVE\n VAR2 e.g. ?bar INTEGER_NEGATIVE\n VAR2 e.g. ?bar DECIMAL_NEGATIVE\n VAR2 e.g. ?bar DOUBLE_NEGATIVE\n VAR2 e.g. ?bar 'TRUE'\n VAR2 e.g. ?bar 'FALSE'\n VAR2 e.g. ?bar BLANK_NODE_LABEL\n VAR2 e.g. ?bar ANON e.g. []\n VAR2 e.g. ?bar NIL e.g. ()\n VAR2 e.g. ?bar '<<'\n VAR2 e.g. ?bar '('\n VAR2 e.g. ?bar '['\n VAR1 e.g. ?foo '{'\n VAR2 e.g. ?bar '{'\n IRIREF e.g. <http://example.com> '{'\n PNAME_LN '{'\n PNAME_NS '{'\n 'a' '{'",
                 range: [
                     {
                         character: 5,
@@ -62,30 +63,26 @@ describe("SPARQL Language Server Extension", () => {
             }
         ]);
     });
-    it("receives hover help from the server", () => __awaiter(this, void 0, void 0, function* () {
+    it("receives hover help from the server", () => __awaiter(void 0, void 0, void 0, function* () {
         const hoverHelp = (yield vscode.commands.executeCommand("vscode.executeHoverProvider", docUri, new vscode.Position(0, 0)));
-        const normalizedHoverHelp = JSON.parse(JSON.stringify(hoverHelp));
-        chai_1.expect(normalizedHoverHelp[0].contents[0].value).to.eql("```\nPrefixDecl\n```");
-        chai_1.expect(normalizedHoverHelp[0].range).to.eql([
-            {
-                line: 0,
-                character: 0
-            },
-            {
-                line: 0,
-                character: 52
-            }
-        ]);
+        const { contents } = hoverHelp[0];
+        const range = hoverHelp[0].range;
+        chai_1.expect(typeof contents[0] === 'string' ? contents[0] : contents[0].value).to.eql("```\nPrefixDecl\n```");
+        chai_1.expect(range.start.line).to.eql(0);
+        chai_1.expect(range.start.character).to.eql(0);
+        chai_1.expect(range.end.line).to.eql(0);
+        chai_1.expect(range.end.character).to.eql(52);
     }));
     // Note that this test and the one above also test error-tolerance, since
     // the language assistance comes after a parse error.
-    it("receives completion suggestions from the server", () => __awaiter(this, void 0, void 0, function* () {
+    it("receives completion suggestions from the server", () => __awaiter(void 0, void 0, void 0, function* () {
         const completions = (yield vscode.commands.executeCommand("vscode.executeCompletionItemProvider", docUri, new vscode.Position(3, 5)));
         const normalizedSuggestedCompletion = JSON.parse(JSON.stringify(completions.items[0]));
         chai_1.expect(normalizedSuggestedCompletion).to.eql({
             label: "<http://www.fakezz.com/fakePrefix>",
             kind: "EnumMember",
             insertText: "<http://www.fakezz.com/fakePrefix>",
+            sortText: "<http://www.fakezz.com/fakePrefix>",
             textEdit: {
                 range: [
                     {
--- vscode-langserver-sparql/src/test/extension.test.ts
+++ vscode-langserver-sparql/src/test/extension.test.ts
@@ -43,8 +43,7 @@ describe("SPARQL Language Server Extension", () => {
         source: "GroupGraphPattern"
       },
       {
-        message:
-          "\tExpected one of the following:\n '^'\n IRIREF e.g. <http://example.com>\n PNAME_LN\n PNAME_NS\n 'a'\n '!'\n '('\n VAR1 e.g. ?foo\n VAR2 e.g. ?bar",
+        message: "\tExpected one of the following:\n '^'\n '!'\n '('\n IRIREF e.g. <http://example.com> '?'\n IRIREF e.g. <http://example.com> '*'\n IRIREF e.g. <http://example.com> '+'\n IRIREF e.g. <http://example.com> '/'\n IRIREF e.g. <http://example.com> '|'\n IRIREF e.g. <http://example.com> VAR1 e.g. ?foo\n IRIREF e.g. <http://example.com> VAR2 e.g. ?bar\n IRIREF e.g. <http://example.com> IRIREF e.g. <http://example.com>\n IRIREF e.g. <http://example.com> PNAME_LN\n IRIREF e.g. <http://example.com> PNAME_NS\n IRIREF e.g. <http://example.com> STRING_LITERAL1\n IRIREF e.g. <http://example.com> STRING_LITERAL2\n IRIREF e.g. <http://example.com> STRING_LITERAL_LONG1\n IRIREF e.g. <http://example.com> STRING_LITERAL_LONG2\n IRIREF e.g. <http://example.com> INTEGER\n IRIREF e.g. <http://example.com> DECIMAL\n IRIREF e.g. <http://example.com> DOUBLE\n IRIREF e.g. <http://example.com> INTEGER_POSITIVE\n IRIREF e.g. <http://example.com> DECIMAL_POSITIVE\n IRIREF e.g. <http://example.com> DOUBLE_POSITIVE\n IRIREF e.g. <http://example.com> INTEGER_NEGATIVE\n IRIREF e.g. <http://example.com> DECIMAL_NEGATIVE\n IRIREF e.g. <http://example.com> DOUBLE_NEGATIVE\n IRIREF e.g. <http://example.com> 'TRUE'\n IRIREF e.g. <http://example.com> 'FALSE'\n IRIREF e.g. <http://example.com> BLANK_NODE_LABEL\n IRIREF e.g. <http://example.com> ANON e.g. []\n IRIREF e.g. <http://example.com> NIL e.g. ()\n IRIREF e.g. <http://example.com> '<<'\n IRIREF e.g. <http://example.com> '('\n IRIREF e.g. <http://example.com> '['\n PNAME_LN '?'\n PNAME_LN '*'\n PNAME_LN '+'\n PNAME_LN '/'\n PNAME_LN '|'\n PNAME_LN VAR1 e.g. ?foo\n PNAME_LN VAR2 e.g. ?bar\n PNAME_LN IRIREF e.g. <http://example.com>\n PNAME_LN PNAME_LN\n PNAME_LN PNAME_NS\n PNAME_LN STRING_LITERAL1\n PNAME_LN STRING_LITERAL2\n PNAME_LN STRING_LITERAL_LONG1\n PNAME_LN STRING_LITERAL_LONG2\n PNAME_LN INTEGER\n PNAME_LN DECIMAL\n PNAME_LN DOUBLE\n PNAME_LN INTEGER_POSITIVE\n PNAME_LN DECIMAL_POSITIVE\n PNAME_LN DOUBLE_POSITIVE\n PNAME_LN INTEGER_NEGATIVE\n PNAME_LN DECIMAL_NEGATIVE\n PNAME_LN DOUBLE_NEGATIVE\n PNAME_LN 'TRUE'\n PNAME_LN 'FALSE'\n PNAME_LN BLANK_NODE_LABEL\n PNAME_LN ANON e.g. []\n PNAME_LN NIL e.g. ()\n PNAME_LN '<<'\n PNAME_LN '('\n PNAME_LN '['\n PNAME_NS '?'\n PNAME_NS '*'\n PNAME_NS '+'\n PNAME_NS '/'\n PNAME_NS '|'\n PNAME_NS VAR1 e.g. ?foo\n PNAME_NS VAR2 e.g. ?bar\n PNAME_NS IRIREF e.g. <http://example.com>\n PNAME_NS PNAME_LN\n PNAME_NS PNAME_NS\n PNAME_NS STRING_LITERAL1\n PNAME_NS STRING_LITERAL2\n PNAME_NS STRING_LITERAL_LONG1\n PNAME_NS STRING_LITERAL_LONG2\n PNAME_NS INTEGER\n PNAME_NS DECIMAL\n PNAME_NS DOUBLE\n PNAME_NS INTEGER_POSITIVE\n PNAME_NS DECIMAL_POSITIVE\n PNAME_NS DOUBLE_POSITIVE\n PNAME_NS INTEGER_NEGATIVE\n PNAME_NS DECIMAL_NEGATIVE\n PNAME_NS DOUBLE_NEGATIVE\n PNAME_NS 'TRUE'\n PNAME_NS 'FALSE'\n PNAME_NS BLANK_NODE_LABEL\n PNAME_NS ANON e.g. []\n PNAME_NS NIL e.g. ()\n PNAME_NS '<<'\n PNAME_NS '('\n PNAME_NS '['\n 'a' '?'\n 'a' '*'\n 'a' '+'\n 'a' '/'\n 'a' '|'\n 'a' VAR1 e.g. ?foo\n 'a' VAR2 e.g. ?bar\n 'a' IRIREF e.g. <http://example.com>\n 'a' PNAME_LN\n 'a' PNAME_NS\n 'a' STRING_LITERAL1\n 'a' STRING_LITERAL2\n 'a' STRING_LITERAL_LONG1\n 'a' STRING_LITERAL_LONG2\n 'a' INTEGER\n 'a' DECIMAL\n 'a' DOUBLE\n 'a' INTEGER_POSITIVE\n 'a' DECIMAL_POSITIVE\n 'a' DOUBLE_POSITIVE\n 'a' INTEGER_NEGATIVE\n 'a' DECIMAL_NEGATIVE\n 'a' DOUBLE_NEGATIVE\n 'a' 'TRUE'\n 'a' 'FALSE'\n 'a' BLANK_NODE_LABEL\n 'a' ANON e.g. []\n 'a' NIL e.g. ()\n 'a' '<<'\n 'a' '('\n 'a' '['\n VAR1 e.g. ?foo VAR1 e.g. ?foo\n VAR1 e.g. ?foo VAR2 e.g. ?bar\n VAR1 e.g. ?foo IRIREF e.g. <http://example.com>\n VAR1 e.g. ?foo PNAME_LN\n VAR1 e.g. ?foo PNAME_NS\n VAR1 e.g. ?foo STRING_LITERAL1\n VAR1 e.g. ?foo STRING_LITERAL2\n VAR1 e.g. ?foo STRING_LITERAL_LONG1\n VAR1 e.g. ?foo STRING_LITERAL_LONG2\n VAR1 e.g. ?foo INTEGER\n VAR1 e.g. ?foo DECIMAL\n VAR1 e.g. ?foo DOUBLE\n VAR1 e.g. ?foo INTEGER_POSITIVE\n VAR1 e.g. ?foo DECIMAL_POSITIVE\n VAR1 e.g. ?foo DOUBLE_POSITIVE\n VAR1 e.g. ?foo INTEGER_NEGATIVE\n VAR1 e.g. ?foo DECIMAL_NEGATIVE\n VAR1 e.g. ?foo DOUBLE_NEGATIVE\n VAR1 e.g. ?foo 'TRUE'\n VAR1 e.g. ?foo 'FALSE'\n VAR1 e.g. ?foo BLANK_NODE_LABEL\n VAR1 e.g. ?foo ANON e.g. []\n VAR1 e.g. ?foo NIL e.g. ()\n VAR1 e.g. ?foo '<<'\n VAR1 e.g. ?foo '('\n VAR1 e.g. ?foo '['\n VAR2 e.g. ?bar VAR1 e.g. ?foo\n VAR2 e.g. ?bar VAR2 e.g. ?bar\n VAR2 e.g. ?bar IRIREF e.g. <http://example.com>\n VAR2 e.g. ?bar PNAME_LN\n VAR2 e.g. ?bar PNAME_NS\n VAR2 e.g. ?bar STRING_LITERAL1\n VAR2 e.g. ?bar STRING_LITERAL2\n VAR2 e.g. ?bar STRING_LITERAL_LONG1\n VAR2 e.g. ?bar STRING_LITERAL_LONG2\n VAR2 e.g. ?bar INTEGER\n VAR2 e.g. ?bar DECIMAL\n VAR2 e.g. ?bar DOUBLE\n VAR2 e.g. ?bar INTEGER_POSITIVE\n VAR2 e.g. ?bar DECIMAL_POSITIVE\n VAR2 e.g. ?bar DOUBLE_POSITIVE\n VAR2 e.g. ?bar INTEGER_NEGATIVE\n VAR2 e.g. ?bar DECIMAL_NEGATIVE\n VAR2 e.g. ?bar DOUBLE_NEGATIVE\n VAR2 e.g. ?bar 'TRUE'\n VAR2 e.g. ?bar 'FALSE'\n VAR2 e.g. ?bar BLANK_NODE_LABEL\n VAR2 e.g. ?bar ANON e.g. []\n VAR2 e.g. ?bar NIL e.g. ()\n VAR2 e.g. ?bar '<<'\n VAR2 e.g. ?bar '('\n VAR2 e.g. ?bar '['\n VAR1 e.g. ?foo '{'\n VAR2 e.g. ?bar '{'\n IRIREF e.g. <http://example.com> '{'\n PNAME_LN '{'\n PNAME_NS '{'\n 'a' '{'",
         range: [
           {
             character: 5,
@@ -66,19 +65,14 @@ describe("SPARQL Language Server Extension", () => {
       "vscode.executeHoverProvider",
       docUri,
       new vscode.Position(0, 0)
-    )) as vscode.Hover;
-    const normalizedHoverHelp = JSON.parse(JSON.stringify(hoverHelp));
-    expect(normalizedHoverHelp[0].contents[0].value).to.eql("```\nPrefixDecl\n```");
-    expect(normalizedHoverHelp[0].range).to.eql([
-      {
-        line: 0,
-        character: 0
-      },
-      {
-        line: 0,
-        character: 52
-      }
-    ]);
+    )) as vscode.Hover[];
+    const { contents } = hoverHelp[0];
+    const range = hoverHelp[0].range as vscode.Range;
+    expect(typeof contents[0] === 'string' ? contents[0] : contents[0].value).to.eql("```\nPrefixDecl\n```");
+    expect(range.start.line).to.eql(0);
+    expect(range.start.character).to.eql(0);
+    expect(range.end.line).to.eql(0);
+    expect(range.end.character).to.eql(52);
   });
 
   // Note that this test and the one above also test error-tolerance, since
@@ -94,6 +88,7 @@ describe("SPARQL Language Server Extension", () => {
       label: "<http://www.fakezz.com/fakePrefix>",
       kind: "EnumMember",
       insertText: "<http://www.fakezz.com/fakePrefix>",
+      sortText: "<http://www.fakezz.com/fakePrefix>",
       textEdit: {
         range: [
           {
