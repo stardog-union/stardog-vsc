cf899026dfdb0dd446de6d6b70547375e929d691
Jason Rogers <jason@stardog.com>
2021-06-22 12:42:50 -0400

    Fix test workspace for GraphQL extension tests

--- vscode-langserver-stardog-graphql/scripts/e2e-tests.sh
+++ vscode-langserver-stardog-graphql/scripts/e2e-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export CODE_TESTS_PATH="$(pwd)/out/test"
-export CODE_TESTS_WORKSPACE="--disable-extensions $(pwd)/fixtures"
+export CODE_TESTS_WORKSPACE="$(pwd)/fixtures"
 export CODE_EXTENSIONS_PATH="$(pwd)/.."
 
 node "$(pwd)/node_modules/vscode/bin/test"
