8e8e302077c9e9d27a55e89b5a15bdf249391c0a
Jason Rogers <jason@stardog.com>
2021-06-25 11:00:59 -0400

    Change testing paradigm for GraphQL to fix those tests on CI

--- vscode-langserver-stardog-graphql/CHANGELOG.md
+++ vscode-langserver-stardog-graphql/CHANGELOG.md
@@ -1,5 +1,11 @@
 # Change Log
 All notable changes to the "vscode-langserver-sparql" extension will be documented in this file.
 
+## [1.0.1]
+- Change testing paradigm to fix tests on CI
+
+## [1.0.0]
+- Add Stardog-specific directives
+
 ## [0.0.1]
 - Initial release
---
+++ vscode-langserver-stardog-graphql/src/test/runTest.ts
@@ -0,0 +1,36 @@
+import * as cp from "child_process";
+import * as path from "path";
+import {
+  downloadAndUnzipVSCode,
+  runTests,
+  resolveCliPathFromVSCodeExecutablePath,
+} from "vscode-test";
+
+async function main() {
+  try {
+    const extensionDevelopmentPath = path.resolve(__dirname, "../../");
+    const extensionTestsPath = path.resolve(__dirname, "./index");
+    const vscodeExecutablePath = await downloadAndUnzipVSCode();
+    const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
+
+    // Use cp.spawn / cp.exec for custom setup
+    cp.spawnSync(cliPath, ["--install-extension", "GraphQL.vscode-graphql", "--force"], {
+      encoding: "utf-8",
+      stdio: "inherit",
+    });
+
+    // Run the extension test
+    await runTests({
+      // Use the specified `code` executable
+      vscodeExecutablePath,
+      // @ts-ignore: vscode-test types aren't right
+      extensionDevelopmentPath,
+      extensionTestsPath,
+    });
+  } catch (err) {
+    console.error("Failed to run tests");
+    process.exit(1);
+  }
+}
+
+main();
